b562d084348b99ab1c36f674f99740737f78b15c
ipavlenko <ipavlenko@inbox.ru>
2016-08-03 18:30:55 +0500

    Structure update

---
+++ src/footer/footer.js
@@ -0,0 +1,8 @@
+(function($, Vue, Core, Widgets) {
+
+    Vue.component('academy-footer', {
+        template: '#academy-footer',
+        mixins: [ Core.WidgetMixin ],
+    });
+
+})(jQuery, Vue, Core, Widgets);
---
+++ src/footer/palette.js
@@ -0,0 +1,44 @@
+(function($, Vue, Core, Widgets) {
+
+    var FooterWidget =
+    Widgets.Widget(Widgets.FootersGroup, Widgets.create({
+        name: 'academy-footer',
+        tag: 'academy-footer',
+        mixins: [ Widgets.WidgetMixin, Widgets.BoxMixin, Widgets.SizeMixin ],
+        props: [
+            { name: 'logo', title: 'Logo', type: 'string', tab: 'content' },
+            { name: 'copy', title: 'Copyright', type: 'string', tab: 'content' },
+            { name: 'email', title: 'Email', type: 'string', tab: 'content' },
+            { name: 'phone', title: 'Phone', type: 'string', tab: 'content' },
+            { name: 'tw', title: 'Twitter', type: 'string', tab: 'content' },
+            { name: 'gl', title: 'Google+', type: 'string', tab: 'content' },
+            { name: 'fb', title: 'Facebook', type: 'string', tab: 'content' },
+            { name: 'vk', title: 'Vkontakte', type: 'string', tab: 'content' },
+        ],
+    }));
+
+    Widgets.Item(Widgets.FootersGroup, {
+        name: 'academy-footer',
+        thumbnail: '/assets/vendor/ntr1x-archery-widgets-academy/src/footer/footer.png',
+        widget: Widgets.build(FooterWidget, {
+            outer: {
+                value: {
+                    background: { value: '#0B3C55' },
+                },
+            },
+            inner: {
+                value: {
+                    margin: { value: '0px auto' },
+                },
+            },
+            width: { value: '1200px' },
+            email: { value: 'info@moscowacademy.com' },
+            phone: { value: '+7 (495) 123-4567' },
+            tw: { value: 'https://twitter.com/bookAgolf' },
+            fb: { value: 'https://ru-ru.facebook.com/bookagolf' },
+            gl: { value: 'https://plus.google.com/communities/102202642996727382512' },
+            copy: { value: `Copyright 2016 © MoscowAcademy.com` },
+        }),
+    });
+
+})(jQuery, Vue, Core, Widgets);
