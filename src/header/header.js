6ba9e96f7e3ba7bbaf540b68a9786622528c2eec
Igor Pavlenko <ipavlenko@inbox.ru>
2016-11-18 15:55:26 +0500

    Versions

--- src/header/header.js
+++ src/header/header.js
@@ -1,8 +1,13 @@
-(function($, Vue, Core, Widgets) {
+(function($, Vue, Core) {
 
     Vue.component('academy-header', {
         template: '#academy-header',
         mixins: [ Core.WidgetMixin ],
+        data: function() {
+            return {
+                entries: this.entries
+            }
+        },
     });
 
-})(jQuery, Vue, Core, Widgets);
+})(jQuery, Vue, Core);
